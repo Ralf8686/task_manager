c530a98dae65e542c5b6f15bc4189afdf0f8ca0d
Semen Levenson <levenson94@gmail.com>
2017-07-18 21:51:21 +0300

    Add delete and edit buttons

--- src/components/AppSelectedBar/AppSelectedBar.js
+++ src/components/AppSelectedBar/AppSelectedBar.js
@@ -4,6 +4,8 @@ import BaseText from '../BaseText/BaseText';
 import AppBarContainer from '../AppBarContainer/AppBarContainer';
 import IconButton from 'material-ui/IconButton';
 import Close from 'material-ui/svg-icons/navigation/close';
+import Create from 'material-ui/svg-icons/content/create';
+import Delete from 'material-ui/svg-icons/action/delete';
 
 const AppBarWrapper = styled.div`
   background: ${({ theme }) => theme.colors.selectedRow};
@@ -24,6 +26,15 @@ export default ({ selected, clearSelect }) =>
       <BaseText type="appTitle">
         {selected.length} SELECTED
       </BaseText>
-      <AppBarControls />
+      <AppBarControls>
+        {selected.length === 1
+          ? <IconButton>
+              <Create />
+            </IconButton>
+          : null}
+        <IconButton>
+          <Delete />
+        </IconButton>
+      </AppBarControls>
     </AppBarContainer>
   </AppBarWrapper>;
