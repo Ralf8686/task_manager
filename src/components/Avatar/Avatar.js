37e4d7df0356e8bbcae522bcb64e0a027c2baeb5
Semen Levenson <levenson94@gmail.com>
2017-07-20 00:08:59 +0300

    Refactor Avatar

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -2,25 +2,31 @@ import React from 'react';
 import Avatar from 'material-ui/Avatar';
 import BaseText from '../BaseText/BaseText';
 
+export const types = {
+  'Payload Monitoring Report': {
+    text: 'PLM',
+    color: '#B3B3B3',
+    bg: '#C5CEFF'
+  },
+  'Tooth Detection Report': {
+    text: 'TD',
+    color: '#FFE8D9',
+    bg: '#FFB482'
+  },
+  'Fragmentation Report': {
+    text: 'FM',
+    color: '#E3F3E4',
+    bg: '#A0D6A5'
+  },
+  Default: {
+    text: '?',
+    color: '#E3F3E4',
+    bg: '#A0D6A5'
+  }
+};
+
 export default ({ type }) => {
-  const types = {
-    'Payload Monitoring Report': {
-      text: 'PLM',
-      color: '#B3B3B3',
-      bg: '#C5CEFF'
-    },
-    'Tooth Detection Report': {
-      text: 'TD',
-      color: '#FFE8D9',
-      bg: '#FFB482'
-    },
-    'Fragmentation Report': {
-      text: 'FM',
-      color: '#E3F3E4',
-      bg: '#A0D6A5'
-    }
-  };
-  const currentType = types[type];
+  const currentType = types[type] || type.Default;
   return (
     <Avatar
       size={42}
