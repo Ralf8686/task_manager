f5fffadb0b12e127e88a5efdc400e2a5d126fb97
Semen Levenson <levenson94@gmail.com>
2017-07-21 01:31:49 +0300

    Make e2e faster

--- e2e/test.js
+++ e2e/test.js
@@ -1,21 +1,23 @@
 module.exports = {
   tags: ['Task list'],
   'Test show api': function(client) {
-    client.url('http://localhost:3001').pause(1000);
+    client.url('http://localhost:3000');
 
     client.expect.element('body').to.be.present.before(1000);
     client.expect.element('.t-task-list').to.be.present.before(1000);
     client.expect.element('#Search').to.be.present.before(1000);
+    client.expect.element('.t-task-item').to.be.present.before(1000);
 
     client.elements('css selector', '.t-task-item', function(result) {
+      console.log(result.value.length);
       if (result.value.length !== 3) {
         throw new Error('No have 3 task items');
       }
     });
   },
   'Filter items': function(client) {
-    client.click('#SearchToggleButton').pause(1000);
-    client.setValue('#Search', 'stub').pause(1000);
+    client.click('#SearchToggleButton');
+    client.setValue('#Search', 'stub');
 
     client.elements('css selector', '.t-task-item', function(result) {
       if (result.value.length !== 2) {
