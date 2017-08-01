30b26255803dc1730393f03c9fee3b6ceae3c86f
Semen Levenson <levenson94@gmail.com>
2017-08-02 01:37:46 +0300

    Add custom assert and relocate selectors to PO

--- e2e/pageObjects/taskList.js
+++ e2e/pageObjects/taskList.js
@@ -1,11 +1,23 @@
 module.exports = {
-  url: 'http://localhost:3001/',
+  url: 'http://localhost:3002/',
   elements: {
     taskList: '.t-task-list',
     taskItem: '.t-task-item',
+    barTitle: '.t-bar-title',
+    barEdit: '.t-bar-edit-button',
     searchControlInput: '#search-control-input',
     searchControlToggle: '.t-search-control-toggle',
-    searchControlClear: '.t-search-control-clear'
+    searchControlClear: '.t-search-control-clear',
+    modalDeleteTask: '.t-modal-delete-task',
+    deleteTask: '.t-bar-delete-task'
+  },
+  sections: {
+    snack: {
+      selector: '.t-snack-delete-task',
+      elements: {
+        undoDelete: 'button'
+      }
+    }
   },
   commands: [
     {
--- e2e/tests/TaskList/test.js
+++ e2e/tests/TaskList/test.js
@@ -1,56 +1,44 @@
 module.exports = {
-  'Show api': function(client) {
+  'Show task list': function(client) {
     var taskList = client.page.taskList();
     taskList.navigate();
-    taskList.expect.element('@taskList').to.be.present.before(1000);
+
     taskList.expect.element('@taskItem').to.be.present.before(1000);
 
-    taskList.getNumberOfItems(function(count) {
-      this.assert.ok(count === 3);
-    });
+    taskList.assert.elementsCount('@taskItem', 3);
   },
   'Filter items': function(client) {
     var taskList = client.page.taskList();
+
     taskList.expect.element('@searchControlInput').to.be.present.before(1000);
     taskList.click('@searchControlToggle');
     taskList.setValue('@searchControlInput', 'stub');
-
-    taskList.getNumberOfItems(function(count) {
-      this.assert.ok(count === 2);
-    });
-
+    taskList.assert.elementsCount('@taskItem', 2);
     taskList.click('@searchControlClear');
-
-    taskList.getNumberOfItems(function(count) {
-      this.assert.ok(count === 3);
-    });
+    taskList.assert.elementsCount('@taskItem', 3);
   },
-  'Select items': function(client) {
+  'Delete item': function(client) {
     var taskList = client.page.taskList();
     taskList.expect.element('@taskItem').to.be.present.before(1000);
-    client.click('.t-task-item:nth-child(1)');
+    taskList.click('@taskItem');
 
-    client.expect.element('.t-bar-title').text.to.equal('1 SELECTED');
-    client.expect.element('.t-bar-delete-task').to.be.present.before(500);
-    client.expect.element('.t-bar-edit-button').to.be.present.before(500);
+    taskList.expect.element('@barTitle').text.to.equal('1 SELECTED');
+    taskList.expect.element('@deleteTask').to.be.present.before(500);
+    taskList.expect.element('@barEdit').to.be.present.before(500);
 
-    client.click('.t-bar-delete-task');
+    taskList.click('@deleteTask');
 
-    client.expect.element('.t-modal-delete-task').to.be.present.before(500);
+    taskList.expect.element('@modalDeleteTask').to.be.present.before(500);
 
-    client.click('.t-modal-delete-task');
+    taskList.click('@modalDeleteTask');
 
-    client.expect.element('.t-snack-delete-task').to.be.present.before(500);
+    taskList.expect.section('@snack').to.be.present.before(500);
 
-    taskList.getNumberOfItems(function(count) {
-      this.assert.ok(count === 2);
-    });
+    taskList.assert.elementsCount('@taskItem', 2);
 
-    client.click('.t-snack-delete-task button');
+    taskList.section.snack.click('@undoDelete');
 
-    taskList.getNumberOfItems(function(count) {
-      this.assert.ok(count === 3);
-    });
+    taskList.assert.elementsCount('@taskItem', 3);
 
     client.end(function() {});
   }
